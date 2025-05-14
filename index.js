ed491450a02655274cc8bfc676eaa1fd5924e32b
samiul-alim12 <nawabitsamiul@gmail.com>
2025-05-14 19:02:41 +0600

    server setup complete

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("this is coffe store server");
+});
+app.listen(port, () => {
+  console.log(`coffe  server port is running in ${port}`);
+});
